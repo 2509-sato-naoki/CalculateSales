c88a3a96e38601473836f11c188283e38b511b53
sato_naoki <sato.naoki@alhinc.jp>
2025-09-12 13:38:46 +0900

    商品定義の追加

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -11,6 +11,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 public class CalculateSales {
 
 	// 支店定義ファイル名
@@ -19,16 +20,24 @@ public class CalculateSales {
 	// 支店別集計ファイル名
 	private static final String FILE_NAME_BRANCH_OUT = "branch.out";
 
+	// 商品定義ファイル名
+	private static final String FILE_NAME_COMMODITY_LIST = "commodity.lst";
+
+	// 商品別集計ファイル名
+	private static final String FILE_NAME_COMMODIYY_OUT = "commodity.out";
+
 	// エラーメッセージ
 	private static final String UNKNOWN_ERROR = "予期せぬエラーが発生しました";
 	private static final String FILE_NOT_EXIST = "支店定義ファイルが存在しません";
+	private static final String COMMODITY_LIST_FILE_NOT_EXIST = "商品定義ファイルが存在しません";
 	private static final String FILE_INVALID_FORMAT = "支店定義ファイルのフォーマットが不正です";
+	private static final String PRODUCT＿FILE_INVALID_FORMAT = "商品定義ファイルのフォーマットが不正です";
 	private static final String FILE_NOT_SERIAL_NUMBER = "売上ファイル名が連番になっていません";
 	private static final String SALE_AMOUNT_OVER_10_DIGIT = "合計金額が10桁を超えました";
 	private static final String INVALID_FORMAT = "のフォーマットが不正です";
 	private static final String INVALID_STORE_CODE = "の支店コードが不正です";
 
- 	/**
+	/**
 	 * メインメソッド
 	 *
 	 * @param コマンドライン引数
@@ -44,9 +53,18 @@ public class CalculateSales {
 		Map<String, String> branchNames = new HashMap<>();
 		// 支店コードと売上金額を保持するMap
 		Map<String, Long> branchSales = new HashMap<>();
+		// 商品コードと商品名を保持するMap
+		Map<String, String> branchProductsNames = new HashMap<>();
+		// 商品コードと売上金額を保持するMap
+		Map<String, Long> branchProductsSales = new HashMap<>();
 
 		// 支店定義ファイル読み込み処理
-		if(!readFile(args[0], FILE_NAME_BRANCH_LST, branchNames, branchSales)) {
+		if (!readFile(args[0], FILE_NAME_BRANCH_LST, branchNames, branchSales)) {
+			return;
+		}
+
+		// 商品定義ファイル読み込み処理
+		if (!readFile(args[0], FILE_NAME_COMMODITY_LIST, branchProductsNames, branchProductsSales)) {
 			return;
 		}
 
@@ -64,7 +82,7 @@ public class CalculateSales {
 		}
 		Collections.sort(rcdFile);
 		// 売り上げファイルが連番か確認する処理
-		for (int i = 0; i < rcdFile.size() -1; i++) {
+		for (int i = 0; i < rcdFile.size() - 1; i++) {
 			int former = Integer.parseInt(rcdFile.get(i).getName().substring(0, 8));
 			int latter = Integer.parseInt(rcdFile.get(i + 1).getName().substring(0, 8));
 
@@ -84,42 +102,45 @@ public class CalculateSales {
 
 				String line;
 				List<String> list = new ArrayList<String>();
-				while((line = br.readLine()) != null) {
+				while ((line = br.readLine()) != null) {
 					list.add(line);
 				}
-				//売上ファイルのフォーマット確認（２行かどうか）をする場所はここ
-				if (list.size() != 2) {
+				//売上ファイルのフォーマット確認（3行かどうか）をする場所はここ
+				if (list.size() != 3) {
 					System.out.println(rcdFile.get(i).getName() + INVALID_FORMAT);
 					return;
 				}
 				// 売上ファイルの⽀店コードが⽀店定義ファイルに存在するか確認する処理はここ
-				if (!branchNames.containsKey(list.get(0))) {
+				if (!branchNames.containsKey(list.get(0)) || !branchProductsNames.containsKey(list.get(1))) {
 					System.out.println(rcdFile.get(i).getName() + INVALID_STORE_CODE);
 					return;
 				}
-				if (!list.get(1).matches("^[0-9]*$")) {
+				if (!list.get(2).matches("^[0-9]*$")) {
 					System.out.println(UNKNOWN_ERROR);
 					return;
 				}
-				Long fileSale = Long.parseLong(list.get(1));
+				Long fileSale = Long.parseLong(list.get(2));
 				Long saleAmount = branchSales.get(list.get(0)) + fileSale;
+				// 該当商品の合計金額
+				Long saleAmountProduct = branchProductsSales.get(list.get(1)) + fileSale;
 				// 合計⾦額が10桁を超えたかどうかの確認
-				if (saleAmount > 10000000000L) {
+				if (saleAmount > 10000000000L || saleAmountProduct > 10000000000L) {
 					System.out.println(SALE_AMOUNT_OVER_10_DIGIT);
 					return;
 				}
 				branchSales.put(list.get(0), saleAmount);
+				branchProductsSales.put(list.get(1), saleAmountProduct);
 
-			} catch(IOException e) {
+			} catch (IOException e) {
 				System.out.println(UNKNOWN_ERROR);
 				return;
 			} finally {
 				// ファイルを開いている場合
-				if(br != null) {
+				if (br != null) {
 					try {
 						// ファイルを閉じる
 						br.close();
-					} catch(IOException e) {
+					} catch (IOException e) {
 						System.out.println(UNKNOWN_ERROR);
 						return;
 					}
@@ -128,7 +149,12 @@ public class CalculateSales {
 		}
 
 		// 支店別集計ファイル書き込み処理
-		if(!writeFile(args[0], FILE_NAME_BRANCH_OUT, branchNames, branchSales)) {
+		if (!writeFile(args[0], FILE_NAME_BRANCH_OUT, branchNames, branchSales)) {
+			return;
+		}
+
+		// 商品別集計ファイル書き込み処理
+		if (!writeFile(args[0], FILE_NAME_COMMODIYY_OUT, branchProductsNames, branchProductsSales)) {
 			return;
 		}
 
@@ -143,15 +169,21 @@ public class CalculateSales {
 	 * @param 支店コードと売上金額を保持するMap
 	 * @return 読み込み可否
 	 */
-	private static boolean readFile(String path, String fileName, Map<String, String> branchNames, Map<String, Long> branchSales) {
+	private static boolean readFile(String path, String fileName, Map<String, String> branchNames,
+			Map<String, Long> branchSales) {
 		BufferedReader br = null;
 
 		try {
 			File file = new File(path, fileName);
-			// 支店定義ファイルがあるかどうか確認
+			// ファイルがあるかどうか確認
 			if (!file.exists()) {
-				System.out.println(FILE_NOT_EXIST);
-				return false;
+				if (fileName == FILE_NAME_BRANCH_LST) {
+					System.out.println(FILE_NOT_EXIST);
+					return false;
+				} else if (fileName == FILE_NAME_COMMODITY_LIST) {
+					System.out.println(COMMODITY_LIST_FILE_NOT_EXIST);
+					return false;
+				}
 			}
 
 			FileReader fr = new FileReader(file);
@@ -159,28 +191,41 @@ public class CalculateSales {
 
 			String line;
 			// 一行ずつ読み込む
-			while((line = br.readLine()) != null) {
+			while ((line = br.readLine()) != null) {
 				// ※ここの読み込み処理を変更してください。(処理内容1-2)
-				String [] storeNameCode = line.split(",");
-				// 支店コードが3桁か、1行に支店コードと支店名が「,」で区切られているか確認
-				if (storeNameCode.length != 2 || !storeNameCode[0].matches("^[0-9]{3}$")) {
-					System.out.println(FILE_INVALID_FORMAT);
-					return false;
+				String[] storeNameCode = line.split(",");
+
+				// 商品定義ファイルの追加における変更点、正規表現、if文、エラー文の出し分け
+				if (fileName == FILE_NAME_BRANCH_LST) {
+					// 支店コードが3桁か、1行に支店コードと支店名が「,」で区切られているか確認
+					if (storeNameCode.length != 2 || !storeNameCode[0].matches("^[0-9]{3}$")) {
+						System.out.println(FILE_INVALID_FORMAT);
+						return false;
+					}
+					branchNames.put(storeNameCode[0], storeNameCode[1]);
+					branchSales.put(storeNameCode[0], 0L);
+
+				} else if (fileName == FILE_NAME_COMMODITY_LIST) {
+					// 商品コードが数字とアルファベット8桁か、1行に支店コードと支店名が「,」で区切られているか確認
+					if (storeNameCode.length != 2 || !storeNameCode[0].matches("[A-Za-z0-9]{8}$")) {
+						System.out.println(PRODUCT＿FILE_INVALID_FORMAT);
+						return false;
+					}
+					branchNames.put(storeNameCode[0], storeNameCode[1]);
+					branchSales.put(storeNameCode[0], 0L);
 				}
-				branchNames.put(storeNameCode[0], storeNameCode[1]);
-				branchSales.put(storeNameCode[0], 0L);
 			}
 
-		} catch(IOException e) {
+		} catch (IOException e) {
 			System.out.println(UNKNOWN_ERROR);
 			return false;
 		} finally {
 			// ファイルを開いている場合
-			if(br != null) {
+			if (br != null) {
 				try {
 					// ファイルを閉じる
 					br.close();
-				} catch(IOException e) {
+				} catch (IOException e) {
 					System.out.println(UNKNOWN_ERROR);
 					return false;
 				}
@@ -198,7 +243,8 @@ public class CalculateSales {
 	 * @param 支店コードと売上金額を保持するMap
 	 * @return 書き込み可否
 	 */
-	private static boolean writeFile(String path, String fileName, Map<String, String> branchNames, Map<String, Long> branchSales) {
+	private static boolean writeFile(String path, String fileName, Map<String, String> branchNames,
+			Map<String, Long> branchSales) {
 		// ※ここに書き込み処理を作成してください。(処理内容3-1)
 		BufferedWriter bw = null;
 
@@ -216,16 +262,16 @@ public class CalculateSales {
 				bw.write(Long.toString(branchSales.get(key)));
 				bw.newLine();
 			}
-		} catch(IOException e) {
+		} catch (IOException e) {
 			System.out.println(UNKNOWN_ERROR);
 			return false;
 		} finally {
 			// ファイルを開いている場合
-			if(bw != null) {
+			if (bw != null) {
 				try {
 					// ファイルを閉じる
 					bw.close();
-				} catch(IOException e) {
+				} catch (IOException e) {
 					System.out.println(UNKNOWN_ERROR);
 					return false;
 				}
